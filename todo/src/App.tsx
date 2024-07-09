097f3d6cf7e0bfa3de108af87c11bb16e79c6695
kimyoda <rladygks1210@naver.com>
2024-07-10 04:04:20 +0900

    [feat] zustand todoStore.jsx add, eventForm, onchange event add

--- todo/src/App.tsx
+++ todo/src/App.tsx
@@ -1,6 +1,12 @@
 import { useState } from "react";
 
 function App() {
+  const [todo, setTodo] = useState("");
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+  };
+
   return (
     <>
       <div className="h-screen w-screen flex justify-center items-center">
@@ -8,12 +14,13 @@ function App() {
           <h1 className="font-bold text-3xl">Todos</h1>
           <p>Add your daily task</p>
 
-          <form action="">
+          <form onSubmit={handleSubmit}>
             <div className="mt-5">
               <input
                 type="text"
                 className="w-full h-10 p-2 rounded-lg bg-[#282828] outline-red-400 border border-red-400"
                 placeholder="할일을 입력해주세요"
+                onChange={(e) => setTodo(e.target.value)}
               />
             </div>
           </form>
